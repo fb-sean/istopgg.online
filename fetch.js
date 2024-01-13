c7438ab3083a8936471a495aa6a0966faf25d00b
fb-sean <sean@sattler.dev>
2024-01-13 23:20:40 +0100

    yes

--- fetch.js
+++ fetch.js
@@ -8,7 +8,7 @@ window.addEventListener('load', () => {
 
         console.debug(body.isOnline ? `Top.gg is currently online.` : `Top.gg is currently offline.`);
 
-        window.location.href = body.isOnline ? 'https://istopgg.online/online' : ('https://istopgg.online/offline' + (body.since ? `?since=${body.since}` : ''));
+        window.location.href = body.isOnline ? 'https://istopgg.online/online' : ('https://istopgg.online/offline' + (body.since ? `?since=${~~(body.since / 1000)}` : ''));
     }).catch(() => {
 
     });
